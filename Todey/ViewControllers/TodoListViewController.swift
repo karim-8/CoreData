be9210aff7914a9a13ebfe96bd7e0bc32ebdae2c
Karim <KariimAhmed8@gmail.com>
2020-06-12 18:28:38 +0200

    #Adding Data model

--- Todey/ViewControllers/TodoListViewController.swift
+++ Todey/ViewControllers/TodoListViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    var staticArr = ["1","2","3"]
+    var staticArr = [Item]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let newItem = Item()
+        newItem.name = "ka"
+        newItem.done = true
+        staticArr.append(newItem)
     }
 
     
@@ -25,21 +29,22 @@ class TodoListViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        
-        cell.textLabel?.text = staticArr[indexPath.row]
-        
+        let item = staticArr[indexPath.row]
+        cell.textLabel?.text = item.name
+        cell.accessoryType = item.done == true ? .checkmark : .none
         return cell
     }
     
     //MARK:- TableView Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //true = false & false = true
+        staticArr[indexPath.row].done = !staticArr[indexPath.row].done
+        tableView.reloadData()
         //Hide shadow while selecting row
         tableView.deselectRow(at: indexPath, animated: true)
-        //adding accessory type
-        tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        
     
     }
     
@@ -50,8 +55,12 @@ class TodoListViewController: UITableViewController {
         
         //adding alert view so user can add new items
         let alert = UIAlertController(title: "Adding New Items", message: "", preferredStyle: .alert)
+        
         let action = UIAlertAction(title: "Add Items", style: .default) { (UIAlertAction) in
-            self.staticArr.append(textField.text!)
+            let newItem = Item()
+            newItem.name = textField.text!
+            
+            self.staticArr.append(newItem)
             self.tableView.reloadData()
         }
         //creating textfield inside alert dialogue
